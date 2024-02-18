10afe3f0d4a185dca539d61e7202098c16167c19
ethanfincher <ethan.j.fincher@gmail.com>
2024-02-18 13:51:38 -0600

    finally fixed python and postman and AWS and my opensearch domain and the internet and windows AND got the GET to work

--- ukraine-python-scripts/CRUDQ_ukraine_script.py
+++ ukraine-python-scripts/CRUDQ_ukraine_script.py
@@ -1,14 +1,23 @@
-command = input('enter an operation: ').lower()
-while(command not in ("c", "r", "u", "d", "q")):
-    command = input('please enter a valid operation: ').lower()
-if command == "c":
-    print("bark")
-elif command == "r":
-    print("herro")
-elif command == "u":
-    print("woof")
-elif command == "d":
-    print("pew pew")
-elif command == "q":
-    print("grrrrr")
-print("end of program")
+import requests
+url = "https://search-newsdata-tester-uy43guqnasvykkyt5wkmx4y7sa.us-east-1.es.amazonaws.com/ukraine_index/_search"
+username = "ethanfincher"
+password = "Password123!"
+response = requests.get(url, auth=(username, password))
+print(response.status_code)
+print(response.json())
+
+
+# if command == "c":
+#     print("bark")
+# elif command == "r":
+#     print("herro")
+# elif command == "u":
+#     print("woof")
+# elif command == "d":
+#     print("pew pew")
+# elif command == "q":
+#     print("grrrrr")
+# command = input('enter an operation: ').lower()
+
+# while(command not in ("c", "r", "u", "d", "q")):
+#     command = input('please enter a valid operation: ').lower()
